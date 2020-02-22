dcdc38c3b96eef8269337f2f8eca835e3ae0f8c3
周泽进 <2211541668@qq.com>
2020-02-22 16:58:38 +0800

    初始化提交

---
+++ src/test/java/com/atguigu/scw/ScwOrderApplicationTests.java
@@ -0,0 +1,18 @@
+package com.atguigu.scw;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@SpringBootApplication	//由于项目是复制粘贴的，测试类生成的配置信息和主类不一致
+public class ScwOrderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
